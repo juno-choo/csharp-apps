f5e5a2e19792a84a6eb1ca9b8efb1d20e10d138e
Juno Choo <choojunhoe@gmail.com>
2025-10-21 18:53:48 -0500

    Initial cm

---
+++ DelegateBasics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DelegateBasicExample
+{
+
+    class Program
+    {
+        delegate void LogDel(string message);
+
+        static void Main(string[] args)
+        {
+            LogDel logDelegate = new LogDel(LogTextToFile);
+
+            System.Console.WriteLine("Please write your name: ");
+            var name = Console.ReadLine();
+            logDelegate(name);
+        }
+
+        static void LogTextToScreen(string message)
+        {
+            Console.WriteLine($"Date: {DateTime.Now}, Message: {message}");
+        }
+        
+        static void LogTextToFile(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), true))
+            {
+                writer.WriteLine($"Date: {DateTime.Now}, Message: {message}");
+            }
+        }
+    }
+}
