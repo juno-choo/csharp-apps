b5e74ad0ddd1e118e89d6e5d23b2fd4fc3cec6a9
Juno Choo <choojunhoe@gmail.com>
2025-10-22 11:00:33 -0500

    Events

---
+++ ThermostatEventsApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ThermostatEventsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implementation for ThermostatEventsApp goes here
+        }
+    }
+
+    public class HeatSensor : IHeatSensor
+    {
+        double _warningLevel;
+        double _emergencyLevel;
+        bool _hasCrossedWarningLevel = false;
+
+        public HeatSensor(double warningLevel, double emergencyLevel)
+        {
+            _warningLevel = warningLevel;
+            _emergencyLevel = emergencyLevel;
+        }
+
+        event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureReachEmergencyLevel
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureAboveWarningLevel
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureBelowWarningLevel
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        public void RunHeatSensor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IHeatSensor
+    {
+        event EventHandler<TemperatureEventArgs> TemperatureReachEmergencyLevel;
+        event EventHandler<TemperatureEventArgs> TemperatureAboveWarningLevel;
+        event EventHandler<TemperatureEventArgs> TemperatureBelowWarningLevel;
+
+        void RunHeatSensor();
+    }
+
+    public class TemperatureEventArgs : EventArgs
+    {
+        public double Temperature { get; set; }
+        public DateTime CurrentDateTime { get; set; }
+    }
+}
