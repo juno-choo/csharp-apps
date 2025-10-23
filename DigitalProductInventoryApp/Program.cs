b3fe964b6a45c236aaba3adf2323310e35e8f285
Juno Choo <choojunhoe@gmail.com>
2025-10-23 18:01:23 -0500

    Factory Design Pattern

---
+++ DigitalProductInventoryApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DigitalProductInventoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    public interface IPrimaryProperties
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+    }
+
+    public static class FactoryPattern<T, U> where T : class, U, new()
+                                             where U : class
+    {
+        public static U GetInstance()
+        {
+            U objT;
+            objT = new T();
+            return objT;
+        }
+    }
+}
